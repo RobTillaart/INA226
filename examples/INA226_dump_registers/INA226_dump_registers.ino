961752d1828ebccd7451455b2fb90463c7a25d75
rob tillaart <rob.tillaart@gmail.com>
2021-06-22 12:58:02 +0200

    add param checking + getShunt() + getMaxCurrent()

--- examples/INA226_dump_registers/INA226_dump_registers.ino
+++ examples/INA226_dump_registers/INA226_dump_registers.ino
@@ -6,12 +6,35 @@
 //    DATE: 2021-06-21
 //     URL: https://github.com/RobTillaart/INA226
 
+/*
+   expected output something like
+
+    REGISTER      VALUE   VALUE_X
+  -----------------------------------
+  CONFIGURATION:  16679   4127
+          SHUNT:  65533   FFFD
+    BUS VOLTAGE:  2       2
+          POWER:  0       0
+        CURRENT:  0       0
+    CALIBRATION:  0       0
+*/
+
 
 #include "INA226.h"
 #include "Wire.h"
 
 INA226 INA(0x40);
 
+
+char names[6][20] =
+{
+  "CONFIGURATION: ",
+  "        SHUNT: ",
+  "  BUS VOLTAGE: ",
+  "        POWER: ",
+  "      CURRENT: ",
+  "  CALIBRATION: "
+};
 
 void setup()
 {
@@ -28,18 +51,18 @@ void setup()
 
 void loop()
 {
-  Serial.println("\n\tREG\tVALUE\tVALUE_X");
+  Serial.println("\n\tREGISTER\tVALUE\tVALUE_X");
   for (int r = 0; r < 6; r++)
   {
     Serial.print('\t');
-    Serial.print(r);
+    Serial.print(names[r]);
     Serial.print('\t');
     Serial.print(INA.getRegister(r), DEC);
     Serial.print('\t');
     Serial.println(INA.getRegister(r), HEX);
   }
   Serial.println();
-  
+
   delay(1000);
 }
 
