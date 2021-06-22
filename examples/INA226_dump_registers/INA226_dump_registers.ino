8b724a6c96a8767ab99ed7516034126b911d7252
Rob Tillaart <rob.tillaart@gmail.com>
2021-06-22 11:36:16 +0200

    Develop (#3)
    * 0.1.1 improve calibration + added some functions

---
+++ examples/INA226_dump_registers/INA226_dump_registers.ino
@@ -0,0 +1,48 @@
+//
+//    FILE: INA226_dump_registers.ino
+//  AUTHOR: Rob Tillaart
+// VERSION: 0.1.0
+// PURPOSE: demo
+//    DATE: 2021-06-21
+//     URL: https://github.com/RobTillaart/INA226
+
+
+#include "INA226.h"
+#include "Wire.h"
+
+INA226 INA(0x40);
+
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println(__FILE__);
+
+  Wire.begin();
+  if (!INA.begin() )
+  {
+    Serial.println("could not connect. Fix and Reboot");
+  }
+}
+
+
+void loop()
+{
+  Serial.println("\n\tREG\tVALUE\tVALUE_X");
+  for (int r = 0; r < 6; r++)
+  {
+    Serial.print('\t');
+    Serial.print(r);
+    Serial.print('\t');
+    Serial.print(INA.getRegister(r), DEC);
+    Serial.print('\t');
+    Serial.println(INA.getRegister(r), HEX);
+  }
+  Serial.println();
+  
+  delay(1000);
+}
+
+
+
+// -- END OF FILE --
